72af085fdb7b6d4e27142ca2bee6d4294246884c
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-17 19:18:51 -0500

    Created Character stats. Added elements to map

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public string name;
+    public int level = 1;
+    public int experience;
+    public int[] expToNextLevel;
+    public int maxLevel = 20;
+
+    public int hp;
+    public int maxHP = 100;
+    public int mp;
+    public int maxMP = 30;
+    public int strength;
+    public int defence;
+    public int power;
+    public int armorClass;
+    public string weapon;
+    public string armor;
+    public Sprite sprite;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
