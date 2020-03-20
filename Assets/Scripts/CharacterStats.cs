12f12e3135a8aee0a5e4af79610eb8006b779d32
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-19 21:01:39 -0500

    Added character leveling system

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -9,13 +9,16 @@ public class CharacterStats : MonoBehaviour
     public int experience;
     public int[] expToNextLevel;
     public int maxLevel = 20;
+    public int baseExp = 250;
 
     public int hp;
     public int maxHP = 100;
+    public int baseMP = 5;
     public int mp;
     public int maxMP = 30;
+    public int[] mpLvlBonus;
     public int strength;
-    public int defence;
+    public int defense;
     public int power;
     public int armorClass;
     public string weapon;
@@ -24,11 +27,64 @@ public class CharacterStats : MonoBehaviour
 
     void Start()
     {
-        
+        mpLvlBonus = new int[maxLevel];
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseExp;
+
+        // Initialize experience to next level and mp.
+        for(int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.5f);
+
+            if(i % 3 == 0)
+            {
+                mpLvlBonus[i] += Mathf.FloorToInt(baseMP * i / 3);
+            }
+        }
     }
 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            AddExp(500);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        experience += expToAdd;
+
+        if(level < maxLevel)
+        {
+            if(experience > expToNextLevel[level])
+            {
+                // Reset experience.
+                experience -= expToNextLevel[level];
+
+                level++;
+            
+                // Determine whether to add to strength or defense.
+                if(level % 2 == 0)
+                {
+                    strength++;
+                }
+                else
+                {
+                    defense++;
+                }
+
+                maxHP = Mathf.FloorToInt(maxHP * 1.05f);
+                hp = maxHP;
+
+                maxMP += mpLvlBonus[level];
+                mp = maxMP;
+            }
+        }
         
+        if(level >= maxLevel)
+        {
+            experience = 0;
+        }
     }
 }
